f41c7532c04c2deabec7351ff198677ba5501c5b
zhehaoli <zhehaoli@mail.ustc.edu.cn>
2024-02-24 16:49:56 +0800

    add smart pointer samples

---
+++ Homeworks/0_cpp_warmup/samples/src/executables/1_BasicDArray_withSmartPointer/DArray.cpp
@@ -0,0 +1,157 @@
+// implementation of class DArray
+#include <iostream>
+
+#include "DArray.h"
+
+#include <cassert>
+
+using namespace std;
+
+// default constructor
+DArray::DArray() {
+	Init();
+}
+
+// set an array with default values
+DArray::DArray(int nSize, double dValue)
+	: m_pData(new double[nSize]), m_nSize(nSize)
+{
+	for (int i = 0; i < nSize; i++)
+		m_pData[i] = dValue;
+}
+
+DArray::DArray(const DArray& arr)
+	: m_pData(new double[arr.m_nSize]), m_nSize(arr.m_nSize)
+{
+	for (int i = 0; i < m_nSize; i++)
+		m_pData[i] = arr.m_pData[i];
+}
+
+// deconstructor
+DArray::~DArray() {
+	// Free(); // Let the smart pointers automatically handle this!
+	m_nSize = 0;
+}
+
+// display the elements of the array
+void DArray::Print() const {
+	cout << "size = " << m_nSize << ":";
+	for (int i = 0; i < m_nSize; i++)
+		cout << " " << GetAt(i);
+
+	cout << endl;
+}
+
+// initilize the array
+void DArray::Init() {
+	m_pData = nullptr;
+	m_nSize = 0;
+}
+
+// get the size of the array
+int DArray::GetSize() const {
+	return m_nSize;
+}
+
+// set the size of the array
+void DArray::SetSize(int nSize) {
+	if (m_nSize == nSize)
+		return;
+
+	std::unique_ptr<double[]> pData(new double[nSize]);
+
+	int copyNum = nSize < m_nSize ? nSize : m_nSize;
+	for (int i = 0; i < copyNum; i++)
+		pData[i] = m_pData[i];
+	for (int i = copyNum; i < nSize; i++)
+		pData[i] = 0.;
+	
+	// The original array pointed by smart pointer m_pData will be automatically released
+	m_pData = std::move(pData);
+	m_nSize = nSize;
+}
+
+// get an element at an index
+const double& DArray::GetAt(int nIndex) const {
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	return m_pData[nIndex];
+}
+
+// set the value of an element 
+void DArray::SetAt(int nIndex, double dValue) {
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	m_pData[nIndex] = dValue;
+}
+
+double& DArray::operator[](int nIndex) {
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	return m_pData[nIndex];
+}
+
+// overload operator '[]'
+const double& DArray::operator[](int nIndex) const {
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	return m_pData[nIndex];
+}
+
+// add a new element at the end of the array
+void DArray::PushBack(double dValue) {
+	std::unique_ptr<double[]> pTemp(new double[static_cast<size_t>(m_nSize) + 1]);
+
+	for (int i = 0; i < m_nSize; i++)
+		pTemp[i] = m_pData[i];
+
+	pTemp[m_nSize] = dValue;
+
+	m_pData = std::move(pTemp);
+	m_nSize++;
+}
+
+// delete an element at some index
+void DArray::DeleteAt(int nIndex) {
+	assert(nIndex >= 0 && nIndex < m_nSize);
+
+	std::unique_ptr<double[]> pTemp(new double[static_cast<size_t>(m_nSize) - 1]);
+
+	for (int i = 0; i < nIndex; i++)
+		pTemp[i] = m_pData[i];
+
+	for (int i = nIndex; i < m_nSize - 1; i++)
+		pTemp[i] = m_pData[i + 1];
+
+	// The original array pointed by smart pointer m_pData will be automatically released
+	m_pData = std::move(pTemp);
+	m_nSize--;
+}
+
+// insert a new element at some index
+void DArray::InsertAt(int nIndex, double dValue) {
+	assert(nIndex>=0 && nIndex <= m_nSize); // nIndex == m_nSize is legal
+	
+	std::unique_ptr<double[]> pTemp(new double[static_cast<size_t>(m_nSize) + 1]);
+
+	for (int i = 0; i < nIndex; i++)
+		pTemp[i] = m_pData[i];
+
+	pTemp[nIndex] = dValue;
+
+	for (int i = nIndex + 1; i < m_nSize + 1; i++)
+		pTemp[i] = m_pData[i - 1];
+
+	// The original array pointed by smart pointer m_pData will be automatically released
+	m_pData = std::move(pTemp);
+	m_nSize++;
+}
+
+// overload operator '='
+DArray& DArray::operator = (const DArray& arr) {
+
+	m_nSize = arr.m_nSize;
+	// Use reset to automatically release the original memory
+	m_pData.reset(new double[m_nSize]);
+
+	for (int i = 0; i < m_nSize; i++)
+		m_pData[i] = arr[i];
+
+	return *this;
+}
---
+++ Homeworks/0_cpp_warmup/samples/src/executables/1_BasicDArray_withSmartPointer/README.md
@@ -0,0 +1,44 @@
+# 智能指针(smart pointers)的使用
+
+在这个sample中，我们使用更加现代的智能指针来管理C++内存。
+
+在之前的练习中，我们都是手动使用`new`与`delete`关键字在堆(heap)中申请与解除内存块的使用权。一旦忘记使用`delete`，就会造成一个变量已经不再使用，但是其内存还未释放的情况（内存泄漏）。
+
+在现代C++中，智能指针可以帮助我们自动跟踪变量的使用情况，在变量生存期结束时自动`delete`， 下面是一个对比：
+
+```c++
+void UseRawPointer()
+{
+    // Using a raw pointer -- not recommended.
+    Song* pSong = new Song(L"Nothing on You", L"Bruno Mars"); 
+
+    // Use pSong...
+
+    // Don't forget to delete!
+    delete pSong;   
+}
+
+void UseSmartPointer()
+{
+    // Declare a smart pointer on stack and pass it the raw pointer.
+    unique_ptr<Song> song2(new Song(L"Nothing on You", L"Bruno Mars"));
+
+    // Use song2...
+    wstring s = song2->duration_;
+    //...
+
+} // song2 is deleted automatically here.
+```
+
+智能指针有三种类型(具体的区别与使用请阅读[这里](https://learn.microsoft.com/en-us/cpp/cpp/smart-pointers-modern-cpp?view=msvc-170#c-standard-library-smart-pointers)): 
++ `unique_ptr`
++ `shared_ptr`
++ `weak_ptr`
+
+想了解更多关于智能指针的信息以及更多的好处，请阅读下面的参考资料。
+
+
+## 参考资料
+1. 详细的介绍：[GeeksForGeeks: smart points](https://www.geeksforgeeks.org/smart-pointers-cpp/)
+2. 简洁的介绍：[智能指针（现代 C++）](https://learn.microsoft.com/zh-cn/cpp/cpp/smart-pointers-modern-cpp?view=msvc-170)
+3. API手册：[cppreference: unique_ptr](https://en.cppreference.com/w/cpp/memory/unique_ptr)
