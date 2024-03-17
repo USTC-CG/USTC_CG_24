a7e1dfe8f92b9cdd4e6379dd0b73253daf071f61
sumaojj <sumaojiang51@gmail.com>
2024-03-17 16:21:20 +0800

    1
    1

--- Homeworks/2_image_warping/documents/eigen_example/src/eigen_example.cpp
+++ Homeworks/2_image_warping/documents/eigen_example/src/eigen_example.cpp
@@ -5,25 +5,27 @@
 using namespace std;
 using namespace Eigen;
 
-int main() {
+int main()
+{
   MatrixXf A(4, 4);
 
   float data[4][4] = {
-	  {1, 0, 0, 0},
-	  {0, 1, 0, 0},
-	  {0, 0, 1, 0},
-	  {0, 0, 0, 1}
-  };
+      {1, 0, 0, 0},
+      {0, 1, 0, 0},
+      {0, 0, 1, 0},
+      {0, 0, 0, 1}};
 
-  for (size_t row = 0; row < 4; row++) {
-	for (size_t col = 0; col < 4; col++)
-	  A(row, col) = data[row][col];
+  for (size_t row = 0; row < 4; row++)
+  {
+    for (size_t col = 0; col < 4; col++)
+      A(row, col) = data[row][col];
   }
 
   MatrixXf b(4, 2);
-  for (size_t row = 0; row < 4; row++) {
-	b(row, 0) = 1;
-	b(row, 1) = 2;
+  for (size_t row = 0; row < 4; row++)
+  {
+    b(row, 0) = 1;
+    b(row, 1) = 2;
   }
 
   MatrixXf x = A.colPivHouseholderQr().solve(b);
