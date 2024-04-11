13917fd4f8df7620970dc88dac8d590b7b048d96
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-04-11 17:27:27 +0800

    Add comment for anisotropic filtering

--- Framework3D/source/RCore/hd_USTC_CG_GL/material.cpp
+++ Framework3D/source/RCore/hd_USTC_CG_GL/material.cpp
@@ -151,6 +151,10 @@ GLuint Hd_USTC_CG_Material::createTextureFromHioImage(const InputDescriptor& des
     }
     glGenerateMipmap(GL_TEXTURE_2D);
 
+    //float aniso = 0.0f;
+    //glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &aniso);
+    //glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, aniso);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
