e3441565b6d8175d90e81cc81f24fb20fe5863d4
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-05-06 22:40:17 +0800

    rm generated file

--- Framework3D/source/nodes/nodes/functions/NODES_FILES_DIR.h
+++
@@ -1,8 +0,0 @@
-#ifndef NODES_FILES_DIR_H
-#define NODES_FILES_DIR_H
-
-#define NODES_FILES_DIR "C:/Users/Jerry/Workspace/USTC_CG_24/Framework3D/source/nodes"
-#define RENDER_NODES_FILES_DIR "C:/Users/Jerry/Workspace/USTC_CG_24/Framework3D/source/nodes/nodes/render"
-#define FUNC_NODES_FILES_DIR "C:/Users/Jerry/Workspace/USTC_CG_24/Framework3D/source/nodes/nodes/functions"
-
-#endif // NODES_FILES_DIR_H
