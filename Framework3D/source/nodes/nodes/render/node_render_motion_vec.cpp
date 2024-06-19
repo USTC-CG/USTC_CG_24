758f921b6de3a7f5655dacab9f0ece9b5eadb8d2
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-06-19 18:46:24 +0800

    WIP

---
+++ Framework3D/source/nodes/nodes/render/node_render_motion_vec.cpp
@@ -0,0 +1,33 @@
+﻿#include "NODES_FILES_DIR.h"
+#include "Nodes/node.hpp"
+#include "Nodes/node_declare.hpp"
+#include "Nodes/node_register.h"
+#include "RCore/Backend.hpp"
+#include "nvrhi/utils.h"
+#include "render_node_base.h"
+#include "resource_allocator_instance.hpp"
+
+namespace USTC_CG::node_render_motion_vec {
+static void node_declare(NodeDeclarationBuilder& b)
+{
+}
+
+static void node_exec(ExeParams params)
+{
+}
+
+static void node_register()
+{
+    static NodeTypeInfo ntype;
+
+    strcpy(ntype.ui_name, "render_motion_vec");
+    strcpy(ntype.id_name, "node_render_motion_vec");
+
+    render_node_type_base(&ntype);
+    ntype.node_execute = node_exec;
+    ntype.declare = node_declare;
+    nodeRegisterType(&ntype);
+}
+
+NOD_REGISTER_NODE(node_register)
+}  // namespace USTC_CG::node_render_motion_vec
