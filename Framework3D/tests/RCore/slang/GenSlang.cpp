4880689c4fc69f1dd739f14fb0601fa740a91efb
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-07-08 00:15:17 +0800

    mtlx

--- Framework3D/tests/RCore/slang/GenSlang.cpp
+++ Framework3D/tests/RCore/slang/GenSlang.cpp
@@ -1,7 +1,14 @@
+#include <MaterialXGenShader/Shader.h>
+
 #include <iostream>
 
+void EmitGlslfxShader(
+    const MaterialX::ShaderGraph& mxGraph,
+    MaterialX::GenContext& mxContext,
+    MaterialX::ShaderStage& mxStage)
+{
 
-
+}
 
 int main()
 {
