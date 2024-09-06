a81d6c717fd899f7d68310cc31570b4e6ea6216b
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-09-06 21:07:16 +0800

    Curve

--- Framework3D/source/GCore/CurveComponent.cpp
+++ Framework3D/source/GCore/CurveComponent.cpp
@@ -7,7 +7,8 @@ std::string CurveComponent::to_string() const
     std::ostringstream out;
     // Loop over the vertices and print the data
     out << "Points component. "
-        << "Vertices count " << vertices.size() << ". Face vertices count "
+        << "Vertices count " << get_vertices().size()
+        << ". Face vertices count "
         << ".";
     return out.str();
 }
@@ -15,15 +16,13 @@ std::string CurveComponent::to_string() const
 GeometryComponentHandle CurveComponent::copy(Geometry* operand) const
 {
     auto ret = std::make_shared<CurveComponent>(operand);
-
-    // This is fast because the VtArray has the copy on write mechanism
-    ret->vertices = this->vertices;
-    ret->width = this->width;
-    ret->displayColor = this->displayColor;
+    copy_prim(curves.GetPrim(), ret->curves.GetPrim());
+    pxr::UsdGeomImageable(curves).MakeInvisible();
     return ret;
 }
 
-CurveComponent::CurveComponent(Geometry* attached_operand) : GeometryComponent(attached_operand)
+CurveComponent::CurveComponent(Geometry* attached_operand)
+    : GeometryComponent(attached_operand)
 {
 }
 
